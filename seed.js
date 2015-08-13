6d9348e91a329b58ce773371de8d83aa029c80f4
Neil Gewirtz <ngewirtz86@gmail.com>
2015-08-13 18:12:26 -0400

    style update

--- seed.js
+++ seed.js
@@ -1,14 +1,14 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('forum.db');
 
-db.run("INSERT INTO categories (name, description) VALUES ('first category', 'this is my first category'), ('second category', 'this is my second category'), ('third category', 'this is my third category')", function (err) {
+db.run("INSERT INTO categories (name, description) VALUES ('Mets', 'All things Mets are discussed here'), ('Jets', 'All things Jets are discussed here'), ('GoT', 'Discuss the TV show and the books. NO SPOILERS!!'), ('Beer', 'Lets talk craft beers and breweries. NO BUD LIGHT!!')", function (err) {
 	if(err){
 		throw err;
 	}
 });
 
 
-db.run("INSERT INTO threads (title, content, cat_id) VALUES ('first thread', 'this is my first thread', 1), ('second thread', 'this is my second thread', 2), ('third thread', 'this is my third thread', 3)", function (err) {
+db.run("INSERT INTO threads (title, content, cat_id, votes) VALUES ('first thread', 'this is my first thread', '1', '0'), ('second thread', 'this is my second thread', '2', '0'), ('third thread', 'this is my third thread', '3', '0'), ('first thread', 'this is my first thread', '4', '0')", function (err) {
 	if(err){
 		throw err;
 	}
