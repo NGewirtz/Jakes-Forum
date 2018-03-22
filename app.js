0571314bfd7eca3853c48f80aaa81d48ea409186
Neil Gewirtz <ngewirtz86@gmail.com>
2018-03-22 14:26:28 -0400

    code cleanup

--- app.js
+++ app.js
@@ -25,11 +25,8 @@ app.get("/forums", function (req, res){
 		if (err) {
 			throw err;
 		}else {
-			var categories = categories
-			//console.log(categories)
 			categories.forEach(function(category){
 				db.get("SELECT * FROM threads WHERE cat_id=? ORDER BY threads.votes DESC", category.id, function (err, thread){
-					//console.log(thread)	
 					threadArray.push(thread)
 					if (threadArray.length === categories.length) {
 						res.render("index.ejs", {categories: categories, thread:threadArray});
@@ -46,7 +43,7 @@ app.get("/forums/threads/new/:id", function (req, res){
 		if(err){
 			throw err;
 		}else {
-			res.render("newcategorie.ejs", {category: category});
+			res.render("newCategory.ejs", {category: category});
 		}
 	});
 });
@@ -61,25 +58,19 @@ app.post("/forums/threads/new/:id", function (req, res){
 		if(err){
 			throw err;
 		}else {
-			res.redirect("/forums/"+id)
+			res.redirect("/forums/"+id);
 		}
 	});
 });
 
 app.get("/forums/:category", function (req, res){
-	var id = req.params.category
+	var id = req.params.category;
 	db.get("SELECT * FROM categories WHERE id=?", id, function (err, category){
 		if(err){
 			throw err;
 		}else {
-			var category = category
 			db.all("SELECT * FROM categories INNER JOIN threads ON threads.cat_id = categories.id", function (err, threads) {
-				//console.log(threads)
 				db.all("SELECT * FROM replies", function (err, replies) {
-					var replies = replies
-					//console.log(replies)
-					//console.log(threads)
-					//if (replies.thread_id === thread.id)
 					res.render("categories.ejs", {category: category, threads: threads, replies: replies})
 				})
 			})
@@ -94,14 +85,13 @@ app.get("/forums/threads/:id", function (req, res){
 			throw err;
 		}else {
 			db.all("SELECT * FROM replies WHERE thread_id=?", id, function (err, reply){
-			db.all("SELECT * FROM nested", function (err, nest) {
-				if (err){
-					throw err;
-				}else {
-					//console.log(reply)
-					res.render("thread.ejs", {thread: thread, reply: reply, nest: nest});
-				}
-			}); 
+				db.all("SELECT * FROM nested", function (err, nest) {
+					if (err){
+						throw err;
+					}else {
+						res.render("thread.ejs", {thread: thread, reply: reply, nest: nest});
+					}
+				});
 			});
 		}
 	});
@@ -110,27 +100,23 @@ app.get("/forums/threads/:id", function (req, res){
 app.post("/forums/threads/:id", function (req, res){
 	var id = req.params.id;
 	var reply = req.body.reply;
-	var location = req.body.location
-	var key = process.env.key
-	console.log(key)
-	// request("http://dev.virtualearth.net/REST/v1/Locations/"+location+"?o=&key="+key, function (error, response, body) {
-	// 	console.log(body)
-	// 	body = JSON.parse(body)
-	// 	if (body.resourceSets[0].resources.length > 0) {
-	// 		var locality = body.resourceSets[0].resources[0].address.adminDistrict
-	// 		console.log(locality)
-	// 	}else{ 
-	// 		var locality = "NY";
-	// 	}
-	var locality = "NY"
-	//console.log(reply)
+	var location = req.body.location;
+	var key = process.env.key;
+	request("http://dev.virtualearth.net/REST/v1/Locations/"+location+"?o=&key="+key, function (error, response, body) {
+		body = JSON.parse(body);
+		if (body.resourceSets[0].resources.length > 0) {
+			var locality = body.resourceSets[0].resources[0].address.adminDistrict
+		}else{
+			var locality = "NY";
+		}
+	var locality = "NY";
 			db.run("INSERT INTO replies (thread_id, content, locality) VALUES (?,?,?)", id, reply, locality, function (err) {
 			if(err){
 				throw err;
 			}
 			res.redirect("/forums/threads/"+id);
 		});
-	// });
+	});
 });
 
 app.put("/forums/threads/:id", function (req, res){
@@ -139,11 +125,8 @@ app.put("/forums/threads/:id", function (req, res){
 		if(err){
 			throw err;
 		}else {
-			//console.log(thread.votes)
 			var votes = thread.votes+1;
-			//console.log(votes)
 			db.run("UPDATE threads SET votes=? WHERE id=?", votes, id, function (err) {
-				//console.log(votes);
 				res.redirect("/forums/threads/"+id)
 			});
 		}
@@ -175,5 +158,5 @@ app.post("/forums/threads/nested/:id", function (req, res) {
 
 
 app.listen(process.env.PORT || 5000, function(){
-	console.log('listening on port 3000!')
+	console.log('listening on port 3000!');
 });
